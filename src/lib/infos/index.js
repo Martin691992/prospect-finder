14fd56655abfb4d44dc81b0d9938bee5cc0eb61e
Martin BONNARD <bonnard.martin@gmail.com>
2025-10-23 10:14:45 +0200

    ajout json

---
+++ src/lib/infos/index.js
@@ -0,0 +1,2 @@
+export { default as codeActivitePrincipale} from './code_insee/code_activite_principal.json'
+export { default as NAF} from './code_naf/naf.json'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { codeActivitePrincipale, NAF} from "$lib/infos"
+    let entreprises = $state()
+    let selectedActivitePrincipale = $state('')
+    let categories = $derived(NAF.filter(line=> line.section == selectedActivitePrincipale))
+    let set_categories = $derived(new Set(categories.map(line=> line.categorie)))
+    $inspect(set_categories)
+    async function callApi() {
+        const response = await fetch('https://recherche-entreprises.api.gouv.fr/search?activite_principale=12.00z&page=1&per_page=20')
+        if(response.status != 200){
+            return {erreur : "Mauvaise requete"}
+        }
+        const data = await response.json()
+        return data.results
+    }
+</script>
+<h1>Recherche de prospect</h1>
+<button type="button" onclick="{() => entreprises = callApi()}">Appel</button>
+
+{#if entreprises}
+{#await entreprises}
+    <p>Chargement...</p>
+{:then entreprises}
+    {#if entreprises.erreur}
+        <p>Erreur : {entreprises.erreur}</p>
+        {:else}
+        {#each entreprises as entreprise}
+            <p>{entreprise.nom_complet}</p>
+        {/each}
+    {/if}
+{/await}
+
+{/if}
+<select bind:value={selectedActivitePrincipale} name="activite_principal" id="activite_principal">
+    <option value="">- - -</option>
+    {#each codeActivitePrincipale as activite}
+        <option value={activite.code}>{activite.label}</option>
+    {/each}
+</select>
+{#if selectedActivitePrincipale}
+    {#each categories as line}
+        <p>{line.code} - {line.categorie} - {line.libelle}</p>
+    {/each}
+{/if}
